3f46c164543d598c9de4b704fa6af05f531983fd
Erik Seyti <erikseyti@gmail.com>
2023-01-09 14:20:29 -0300

    setup project to have postgres as database instead of sqlite

--- todo_app/database.py
+++ todo_app/database.py
@@ -2,10 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+postgres_env = os.getenv('postgres_env')
+
+SQLALCHEMY_DATABASE_URL = postgres_env
 
 engine = create_engine(
-  SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+  SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
