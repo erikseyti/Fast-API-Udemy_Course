4015ffa6641030bc23987572fae9c78f2ac0224b
Erik Seyti <erikseyti@gmail.com>
2023-01-09 15:41:29 -0300

    Add External APIs as a router

--- todo_app/main.py
+++ todo_app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import models
 from database import  engine
 from routers import auth, todos
+from company import companyapis
 
 app = FastAPI()
 
@@ -9,4 +10,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-
+app.include_router(
+  companyapis.router,
+  prefix='/companyapis',
+  tags=['companyapis'],
+  responses={410: {'description': 'Internal Use Only :"( '}}
+)
